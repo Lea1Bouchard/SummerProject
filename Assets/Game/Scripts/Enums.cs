205056b7a830568a280fc11519e5a8369dcb4dfd
Lea1Bouchard <lbouchar14@etu.uqac.ca>
2023-05-29 13:38:32 -0400

    Style: Added some comments

---
+++ Assets/Game/Scripts/Enums.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Use those enums by include "using Enums" in file
+namespace Enums
+{
+    //Elements
+    public enum Elements
+    {
+        Fire,
+        Water,
+        Air,
+        Earth,
+        Lightning,
+        Ice,
+        Light,
+        Darkness
+    }
+
+    //EnemyTypes
+    public enum EnemyType
+    {
+        Golem,
+        Scarab,
+        Slime,
+        NightmareDragon
+    }
+
+    //GameState
+    public enum GameState
+    {
+        InMenu,
+        InGame,
+        InWaitMode,
+        InFight
+    }
+
+    //MovementState
+    public enum MovementState
+    {
+        Idle,
+        Walking,
+        Running,
+        Falling,
+        Jumping,
+        OnHoverboard
+    }
+
+    //TypeOfAbility
+    public enum TypeOfAbility
+    {
+        Movement,
+        Combat
+    }
+
+    //FightingState
+    public enum FightingState
+    {
+        Attacking,
+        Teleporting,
+        Dodging,
+        Moving,
+        None
+    }
+}

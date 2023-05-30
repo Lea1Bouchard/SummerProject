e921b1cd09a88e4a90d34574210f0682a0a49e60
Lea1Bouchard <lbouchar14@etu.uqac.ca>
2023-05-30 13:49:31 -0400

    Feat: Added some keybinds and fixed character walk

--- Assets/Game/Scripts/GameManager.cs
+++ Assets/Game/Scripts/GameManager.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Enums;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
+
+    [Header("Inputs")]
+    public InputActionAsset inputActions;
+    private InputAction waitModeAction;
+
+    [Header("UIs")]
+    [SerializeField] private Canvas waitModeUI;
 
     public static GameManager Instance
     {
@@ -19,5 +28,21 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         _instance = this;
+        waitModeAction = inputActions.FindActionMap("Player").FindAction("WaitMode");
+    }
+
+    public void InWaitMode(InputAction.CallbackContext context)
+    {
+        Debug.Log("InWaitMode");
+    }
+
+    private void OnEnable()
+    {
+        inputActions.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.FindActionMap("Player").Disable();
     }
 }

860e16991551f86943b08a7e2ffea6e191985c65
Falerhon <alex-lex360@hotmail.com>
2023-06-05 11:31:43 -0400

    Fix : privated unecessary public var

--- Assets/Game/Scripts/AbilitySystem/RangedAbility/RangedAbility.cs
+++ Assets/Game/Scripts/AbilitySystem/RangedAbility/RangedAbility.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Abilities/RangedAbility")]
 public class RangedAbility : Ability
 {
-    public float range = 5f;
+    public float timeToLive = 5f;
     public float speed = 1f;
     public float force = 1f;
     public GameObject projectile;
-    public GameObject projectileClone;
+    private GameObject projectileClone;
     private Projectile projectileScript;
 
     public override void Initialize(Characters ini)
-    {
+    { 
         isActive = false;
         initiator = ini;
 
@@ -42,7 +42,7 @@ public class RangedAbility : Ability
         projectileScript = projectileClone.GetComponent<Projectile>();
 
         projectileScript.Damage = baseDamage;
-        projectileScript.Range = range;
+        projectileScript.Range = timeToLive;
         projectileScript.Speed = speed;
         projectileScript.Force = force;
         projectileScript.Initiator = Initiator;

416ec580a0a303acab23bb7852cccfe6ca0c86aa
Falerhon <alex-lex360@hotmail.com>
2023-06-12 13:58:58 -0400

    Fix : Character able to teleport trough walls

--- Assets/Game/Scripts/AbilitySystem/MovementAbility.cs
+++ Assets/Game/Scripts/AbilitySystem/MovementAbility.cs
@@ -52,20 +52,15 @@ public class MovementAbility : Ability
             Characters hitCharacter = solid.collider.gameObject.GetComponent<Characters>();
             if (hitCharacter != null)
             {
-                Debug.Log("Character : " + hitCharacter.name);
-                //AFTER THIS IS WHERE IT'S NOT WORKING
-                Debug.DrawRay((solid.point + initiator.transform.forward * 5) + new Vector3(0, 1, 0), initiator.transform.position - solid.point, Color.red, 100);
-                Debug.DrawRay(solid.point + new Vector3(0, 1.5f, 0), initiator.transform.position - solid.point , Color.cyan, 100);
+              
                 if (Physics.Raycast(solid.point + initiator.transform.forward * 5, initiator.transform.position - solid.point, hitInfo: out exit))
                     for (int x = 0; x <= 5; x++)
                     {
-                        Debug.Log("Point : " + exit.point);
+                        
                         if (exit.collider.gameObject.GetComponent<Characters>() != null)
                         {
                             if (exit.collider.gameObject.GetComponent<Characters>() == hitCharacter)
                             {
-                                Debug.Log("Enter point : " + solid.point);
-                                Debug.Log("Exit point : " + exit.point);
                                 break;
                             }
 
@@ -79,9 +74,9 @@ public class MovementAbility : Ability
 
                     blinkExit = exit.point + (initiator.transform.forward);
 
-                    Physics.Raycast(exit.point, blinkExit - exit.point, out safeDistance, maxDistance: 2);
-                    Debug.Log(safeDistance.point);
-                    Debug.DrawRay(exit.point, exit.point - blinkExit, Color.yellow, 100);
+                    Physics.Raycast(exit.point + new Vector3(0, 1, 0), blinkExit - exit.point, out safeDistance, maxDistance: 2);
+                    Debug.Log("Safedistance hit : " + safeDistance.point);
+                    Debug.DrawRay(exit.point + new Vector3(0, 1, 0), blinkExit - exit.point, Color.yellow, 100);
                     if (safeDistance.point == Vector3.zero)
                     {
                         controller.enabled = false;
@@ -93,6 +88,8 @@ public class MovementAbility : Ability
                 
 
             }
+
+            Debug.Log("Hit : " + solid.point);
         }
 
         abilityCooldownClass.Initialize(this);
@@ -105,27 +102,4 @@ public class MovementAbility : Ability
         abilityCooldownClass.Initialize(this);
     }
 
-    /* if (Physics.Linecast(initiator.transform.position, destination, out solid))
-        {
-            Characters hitCharacter = solid.collider.gameObject.GetComponent<Characters>();
-            if (hitCharacter != null)
-            {
-                if (Physics.Linecast(solid.point, destination + Vector3.one * 10, out exit))
-                {
-
-                    Debug.Log("Enter point : " + solid.point);
-                    Debug.Log("Exit point : " + exit.point);
-
-                    Vector3 blinkExit;
-
-                    blinkExit = exit.point + (initiator.transform.forward * 2);
-
-                    controller.enabled = false;
-                    initiator.gameObject.transform.position = blinkExit;
-                    controller.enabled = true;
-
-
-                }
-            }
-        }*/
 }
